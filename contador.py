d93f28d113668ef277ba9911d47f134b2b4548a6
Tiagolorenzo <tiago.lorenzo@hotmail.com>
2024-05-21 19:55:45 -0300

    contagem regressiva

---
+++ contador.py
@@ -0,0 +1,9 @@
+# usuário informa o numero
+
+contador = int(input('Informe um número inteiro: '))
+
+# conta a partir do numero informado até zero
+
+while contador >= 0:
+    print(contador)
+    contador -= 1
