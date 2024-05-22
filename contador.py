3feed8697bac3370f61bf1a5334d382695929062
Tiagolorenzo <tiago.lorenzo@hotmail.com>
2024-05-21 21:06:40 -0300

    adicionei contagem por segundo

--- contador.py
+++ contador.py
@@ -1,9 +1,18 @@
 # usuário informa o numero
+# importar bibliotecas
+
+import os
+import time
 
 contador = int(input('Informe um número inteiro: '))
+os.system('cls')
 
 # conta a partir do numero informado até zero
 
 while contador >= 0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
+
+print('BOOM!!!')
